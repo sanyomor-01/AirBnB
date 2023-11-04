e11f9ba8c91561074e6cf21e6f48b732e0ab39c1
sanyomor-01 <sanyomormichael@gmail.com>
2023-11-04 18:47:24 +0000

    card items added using props

--- airbnb-experience/src/App.jsx
+++ airbnb-experience/src/App.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import Nav from "./Components/Nav"
 import Hero from "./Components/Hero"
 import Card from "./Components/Card"
+import data from "./data"
 import './App.css'
 
 function App() {
 
+  const cardElements = data.map( (data) =>{
+    return <Card
+     img = {data.coverImg}
+     title = {data.title}
+     price = {data.price}
+     rating ={data.stats.rating}
+     location ={data.location}reviewCount = {data.stats.reviewCount}
+
+
+    
+    />
+  } )
   return (
     
       <div>
        <Nav/>
        <Hero/>
-      <Card/>
+      {cardElements}
       </div> 
   )
 }
--- airbnb-experience/src/Components/Card.jsx
+++ airbnb-experience/src/Components/Card.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import katie from "../../public/assets/katie-zaferes.png"
-import star from "../../public/assets/star.png"
 
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={katie} alt="katie pic" className='card--image'/>
+            <img src={`../../public/assets/${props.img}`} alt="katie pic" className='card--image'/>
 
             <div className="card--stats">
-                <img src={star} alt="rate star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <img src="../../public/assets/star.png" alt="rate star" />
+                <span> {props.rating} </span>
+                <span className="gray">{props.reviewCount} • </span>
+                <span className="gray">{props.location} </span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / Person</p>
+            <p> {props.title} </p>
+            <p><span className="bold">From ${props.price}</span> / Person</p>
 
         </div>
     )
